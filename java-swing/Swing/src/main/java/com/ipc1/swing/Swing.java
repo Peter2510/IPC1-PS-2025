b76b24ed7fb4f01f246cd44864fc16870ce3bbb2
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2025-03-31 02:42:07 -0600

    Agregando presentación y ejemplo java swing

---
+++ java-swing/Swing/src/main/java/com/ipc1/swing/Swing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ipc1.swing;
+
+/**
+ *
+ * @author peterg
+ */
+public class Swing {
+
+    public static void main(String[] args) {
+        VentanaPrincipal v1 = new VentanaPrincipal();
+        v1.setVisible(true);
+    }
+}
